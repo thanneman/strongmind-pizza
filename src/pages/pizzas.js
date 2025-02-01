bd80e65e61778353f3f950dd29dafada4f24b391
Travis Hanneman <thanneman1@gmail.com>
2025-01-31 18:03:25 -0700

    feat: add check if pizza with topping combo exist

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -23,13 +23,19 @@ export default function Pizzas() {
     const [newPizza, setNewPizza] = useState({ name: '', toppings: [] })
     const [availableToppings, setAvailableToppings] = useState(initialToppings)
     const [editingId, setEditingId] = useState(null)
-    const [editingName, setEditingName] = useState('')
     const [isDialogOpen, setIsDialogOpen] = useState(false);
 
     const addPizza = () => {
         if (newPizza.name.trim() === '') return
         if (pizzas.some((pizza) => pizza.name.toLowerCase() === newPizza.name.toLowerCase())) {
-            alert('Pizza already exists')
+            alert('Pizza name already exists')
+            return
+        }
+        if (pizzas.some((pizza) => 
+            pizza.toppings.length === newPizza.toppings.length &&
+            pizza.toppings.every((topping) => newPizza.toppings.includes(topping))
+        )) {
+            alert('Pizza with these toppings already exists')
             return
         }
         setPizzas([...pizzas, { id: pizzas.length + 1, ...newPizza }])
@@ -79,19 +85,24 @@ export default function Pizzas() {
         <main className='container max-w-4xl py-6'>
             <div className='flex flex-col mt-10 gap-y-6'>
                 <h1 className='self-center text-2xl font-semibold'>Manage Pizzas</h1>
-                <button onClick={() => openDialog()} className='px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600'>Create New Pizza</button>
-                <div className='flex flex-col mt-2 gap-y-3'>
-                    <h2 className='pb-1 text-xl font-medium border-b border-zinc-200'>Available Pizzas</h2>
-                    <ul className='flex flex-col w-full gap-y-1'>
+                <button onClick={() => openDialog()} className='flex items-center justify-center px-4 py-2 text-white bg-blue-500 rounded gap-x-0.5 items hover:bg-blue-600'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24"><path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"></path></svg>
+                    Create New Pizza
+                </button>
+                <div className='flex flex-col mt-5 gap-y-3'>
+                    <h2 className='text-xl font-medium text-center'>Available Pizzas</h2>
+                    <ul className='flex flex-col w-full gap-y-2.5'>
                         {pizzas.map((pizza) => (
-                            <div key={pizza.id} className='p-4 border rounded shadow'>
+                            <div key={pizza.id} className='p-4 border rounded shadow-sm'>
                                 <div className='flex justify-between'>
                                     <h3 className='text-lg font-semibold'>{pizza.name}</h3>
                                     <div className='flex gap-x-2'>
-                                        <button onClick={() => openDialog(pizza)} className='px-2 py-1 text-white bg-yellow-500 rounded hover:bg-yellow-600'>
+                                        <button onClick={() => openDialog(pizza)} className='flex justify-center items-center gap-x-0.5 px-2 py-1 text-white bg-yellow-500 rounded hover:bg-yellow-600'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24"><path fill="currentColor" d="M5 19h1.425L16.2 9.225L14.775 7.8L5 17.575zm-1 2q-.425 0-.712-.288T3 20v-2.425q0-.4.15-.763t.425-.637L16.2 3.575q.3-.275.663-.425t.762-.15t.775.15t.65.45L20.425 5q.3.275.437.65T21 6.4q0 .4-.138.763t-.437.662l-12.6 12.6q-.275.275-.638.425t-.762.15zM19 6.4L17.6 5zm-3.525 2.125l-.7-.725L16.2 9.225z"></path></svg>
                                             Edit
                                         </button>
-                                        <button onClick={() => deletePizza(pizza.id)} className='px-2 py-1 text-white bg-red-500 rounded hover:bg-red-600'>
+                                        <button onClick={() => deletePizza(pizza.id)} className='flex justify-center items-center gap-x-0.5 px-2 py-1 text-white bg-red-500 rounded hover:bg-red-600'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6q-.425 0-.712-.288T4 5t.288-.712T5 4h4q0-.425.288-.712T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5t-.288.713T19 6v13q0 .825-.587 1.413T17 21zm3-4q.425 0 .713-.288T11 16V9q0-.425-.288-.712T10 8t-.712.288T9 9v7q0 .425.288.713T10 17m4 0q.425 0 .713-.288T15 16V9q0-.425-.288-.712T14 8t-.712.288T13 9v7q0 .425.288.713T14 17"></path></svg>
                                             Delete
                                         </button>
                                     </div>
@@ -107,7 +118,6 @@ export default function Pizzas() {
                     </ul>
                 </div>
             </div>
-
             {isDialogOpen && (
                 <div className='fixed inset-0 flex justify-center item-center bg-black/50'>
                     <div className='absolute flex flex-col w-1/2 p-6 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded shadow-md top-1/2 left-1/2'>
